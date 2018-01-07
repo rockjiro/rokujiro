7e37847501cf60398807b96b9dc43536d5e7fb3c
Takumi Ozato <1992tkm0819@gmail.com>
2018-01-07 00:27:04 +0000

    wp-configの設定

--- wp-config.php
+++ wp-config.php
@@ -91,7 +91,6 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
-
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'){
     $_SERVER['HTTPS'] = 'on';
 }
