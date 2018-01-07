56e15c1a136aa9b4fee76429f8fd8f497333ab46
Takumi Ozato <1992tkm0819@gmail.com>
2018-01-07 12:42:38 +0000

    ソーシャルナビゲーションを削除

--- wp-content/themes/nucleare/header.php
+++ wp-content/themes/nucleare/header.php
@@ -42,45 +42,6 @@
 				$snapchatURL = get_theme_mod('nucleare_theme_options_snapchaturl', '');
 			?>
 			<div class="theNavigationSocial">
-				<?php if (!empty($facebookURL)) : ?>
-					<a href="<?php echo esc_url($facebookURL); ?>" title="<?php esc_attr_e( 'Facebook', 'nucleare' ); ?>"><i class="fa fa-facebook"><span class="screen-reader-text"><?php esc_html_e( 'Facebook', 'nucleare' ); ?></span></i></a>
-				<?php endif; ?>
-				<?php if (!empty($twitterURL)) : ?>
-					<a href="<?php echo esc_url($twitterURL); ?>" title="<?php esc_attr_e( 'Twitter', 'nucleare' ); ?>"><i class="fa fa-twitter"><span class="screen-reader-text"><?php esc_html_e( 'Twitter', 'nucleare' ); ?></span></i></a>
-				<?php endif; ?>
-				<?php if (!empty($googleplusURL)) : ?>
-					<a href="<?php echo esc_url($googleplusURL); ?>" title="<?php esc_attr_e( 'Google Plus', 'nucleare' ); ?>"><i class="fa fa-google-plus"><span class="screen-reader-text"><?php esc_html_e( 'Google Plus', 'nucleare' ); ?></span></i></a>
-				<?php endif; ?>
-				<?php if (!empty($linkedinURL)) : ?>
-					<a href="<?php echo esc_url($linkedinURL); ?>" title="<?php esc_attr_e( 'Linkedin', 'nucleare' ); ?>"><i class="fa fa-linkedin"><span class="screen-reader-text"><?php esc_html_e( 'Linkedin', 'nucleare' ); ?></span></i></a>
-				<?php endif; ?>
-				<?php if (!empty($instagramURL)) : ?>
-					<a href="<?php echo esc_url($instagramURL); ?>" title="<?php esc_attr_e( 'Instagram', 'nucleare' ); ?>"><i class="fa fa-instagram"><span class="screen-reader-text"><?php esc_html_e( 'Instagram', 'nucleare' ); ?></span></i></a>
-				<?php endif; ?>
-				<?php if (!empty($youtubeURL)) : ?>
-					<a href="<?php echo esc_url($youtubeURL); ?>" title="<?php esc_attr_e( 'YouTube', 'nucleare' ); ?>"><i class="fa fa-youtube"><span class="screen-reader-text"><?php esc_html_e( 'YouTube', 'nucleare' ); ?></span></i></a>
-				<?php endif; ?>
-				<?php if (!empty($pinterestURL)) : ?>
-					<a href="<?php echo esc_url($pinterestURL); ?>" title="<?php esc_attr_e( 'Pinterest', 'nucleare' ); ?>"><i class="fa fa-pinterest"><span class="screen-reader-text"><?php esc_html_e( 'Pinterest', 'nucleare' ); ?></span></i></a>
-				<?php endif; ?>
-				<?php if (!empty($tumblrURL)) : ?>
-					<a href="<?php echo esc_url($tumblrURL); ?>" title="<?php esc_attr_e( 'Tumblr', 'nucleare' ); ?>"><i class="fa fa-tumblr"><span class="screen-reader-text"><?php esc_html_e( 'Tumblr', 'nucleare' ); ?></span></i></a>
-				<?php endif; ?>
-				<?php if (!empty($vkURL)) : ?>
-					<a href="<?php echo esc_url($vkURL); ?>" title="<?php esc_attr_e( 'VK', 'nucleare' ); ?>"><i class="fa fa-vk"><span class="screen-reader-text"><?php esc_html_e( 'VK', 'nucleare' ); ?></span></i></a>
-				<?php endif; ?>
-				<?php if (!empty($stumbleuponURL)) : ?>
-					<a href="<?php echo esc_url($stumbleuponURL); ?>" title="<?php esc_attr_e( 'Stumbleupon', 'nucleare' ); ?>"><i class="fa fa-stumbleupon"><span class="screen-reader-text"><?php esc_html_e( 'Stumbleupon', 'nucleare' ); ?></span></i></a>
-				<?php endif; ?>
-				<?php if (!empty($snapchatURL)) : ?>
-					<a href="<?php echo esc_url($snapchatURL); ?>" title="<?php esc_attr_e( 'Snapchat', 'nucleare' ); ?>"><i class="fa fa-snapchat"><span class="screen-reader-text"><?php esc_html_e( 'Snapchat', 'nucleare' ); ?></span></i></a>
-				<?php endif; ?>
-				<?php if ($hideRss == 1 ) : ?>
-					<a href="<?php bloginfo( 'rss2_url' ); ?>" title="<?php esc_attr_e( 'RSS', 'nucleare' ); ?>"><i class="fa fa-rss"><span class="screen-reader-text"><?php esc_html_e( 'RSS', 'nucleare' ); ?></span></i></a>
-				<?php endif; ?>
-				<?php if ($hideSearch == 1 ) : ?>
-					<div id="open-search" class="top-search"><i class="fa fa-search"><span class="screen-reader-text"><?php esc_html_e( 'Search', 'nucleare' ); ?></span></i></div>
-				<?php endif; ?>
 			</div>
 		</div>
 	</div>
