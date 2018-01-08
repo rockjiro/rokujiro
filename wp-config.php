c117d4bd956f0b8d021775e7b7e208925460830d
Takumi Ozato <1992tkm0819@gmail.com>
2018-01-08 09:33:08 +0000

    wp-configの編集

--- wp-config.php
+++ wp-config.php
@@ -91,9 +91,9 @@ define('WP_HOME','http://dolphin-rokujiro.com/');
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
-/** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
-
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'){
     $_SERVER['HTTPS'] = 'on';
 }
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
