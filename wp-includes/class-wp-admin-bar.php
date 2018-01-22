2a51c16072401c63692c032dd2d360ef4ccb01e0
Takumi Ozato <1992tkm0819@gmail.com>
2018-01-22 13:36:13 +0000

    スタイルを変更

--- wp-includes/class-wp-admin-bar.php
+++ wp-includes/class-wp-admin-bar.php
@@ -412,7 +412,7 @@ class WP_Admin_Bar {
 		?>
 		<div id="wpadminbar" class="<?php echo $class; ?>">
 			<?php if ( ! is_admin() ) { ?>
-				<a class="screen-reader-shortcut" href="#wp-toolbar" tabindex="0"><?php _e( 'Skip to toolbar' ); ?></a>
+				<a class="screen-reader-shortcut" href="#wp-toolbar" tabindex="1"><?php _e( 'Skip to toolbar' ); ?></a>
 			<?php } ?>
 			<div class="quicklinks" id="wp-toolbar" role="navigation" aria-label="<?php esc_attr_e( 'Toolbar' ); ?>" tabindex="0">
 				<?php foreach ( $root->children as $group ) {
