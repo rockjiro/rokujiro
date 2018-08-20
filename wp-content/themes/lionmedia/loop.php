9d67ba6c7ad7c464cc356c65322a5227c1f8aaa8
Takumi Ozato <1992tkm0819@gmail.com>
2018-08-20 13:15:13 +0000

    記事一覧のカラム数を変更

--- wp-content/themes/lionmedia/loop.php
+++ wp-content/themes/lionmedia/loop.php
@@ -7,14 +7,18 @@
 	  ?>
       <article class="archiveItem">
         
-        <div class="eyecatch eyecatch-archive">
+        <div class="eyecatch eyecatch-archive" style="width: 40%;">
           <?php if(is_sticky()):?>
             <span class="eyecatch__ribbon">Pickup</span>
           <?php endif;?>
           <span class="eyecatch__cat bgc<?php if (isset($cat_meta[$cat_id])) { echo esc_html($cat_meta[$cat_id]);} ?> u-txtShdw"><?php the_category(' ');?></span>
           <a href="<?php the_permalink(); ?>"><?php if(has_post_thumbnail()) {the_post_thumbnail('icatch');} else {echo '<img src="'.get_template_directory_uri().'/img/img_no.gif" alt="NO IMAGE"/>';}?></a>
         </div>
-        
+    
+        <div style="width: 58%;">
+        <h2 class="heading heading-archive">
+          <a class=" hc<?php if (isset($cat_meta[$cat_id])) { echo esc_html($cat_meta[$cat_id]);} ?>" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </h2>    
         <ul class="dateList dateList-archive">
           <li class="dateList__item icon-calendar"><?php the_time('Y.m.d'); ?></li>
           <?php if(has_tag()==true) :  ?>
@@ -34,13 +38,6 @@
 		  ?></li>
           <?php endif; ?>
         </ul>
-
-        <h2 class="heading heading-archive">
-          <a class=" hc<?php if (isset($cat_meta[$cat_id])) { echo esc_html($cat_meta[$cat_id]);} ?>" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-        </h2>
-        
-        <p class="archiveItem__text u-none-sp">
-		  <?php echo get_the_excerpt(); ?>
-        </p>
+        </div>
       
       </article>
